bbda147540e7cf68af4344c29631a6e1078a5e75
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-11 06:06:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,7 @@
-def# Write your code here.
-batch_badge_creator = []
-badge_maker.each (name) do
+def
+batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+badge_maker.each || do
 puts "Hello ,my name is #{name}"
 badge_maker("rokhaya")
 end
