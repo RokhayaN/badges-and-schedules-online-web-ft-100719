e3e7ff9ed3fc0b9df06585a23d382ca81e36dc60
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-11 06:17:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,8 @@
 def
 batch_badge_creator = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 badge_maker.each |name| do
-  puts "Hello ,my name is #{name}"
+  puts "Hello ,my name is #
+  {name}|"
   name = batch_badge_creator += 1
   end
 end
